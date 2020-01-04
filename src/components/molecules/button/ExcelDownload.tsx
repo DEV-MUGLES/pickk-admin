f5c13de846c57e8eb83c40bded8532aef3c5c903
Choi Sumin <greatSumini@gmail.com>
2020-01-05 05:11:36 +0900

    [Feature] add ExcelDownloadButton
    Resolve #2

---
+++ src/components/molecules/button/ExcelDownload.tsx
@@ -0,0 +1,42 @@
+import { Button } from "antd";
+import ReactExport from "react-export-excel";
+import { getDateTimeStrings } from "@src/lib/DateParser";
+import { addLeadingZeros } from "@src/lib/NumberParser";
+
+const { ExcelFile, ExcelSheet, ExcelColumn } = ReactExport;
+
+export type ExcelDownloadButtonProps = {
+  title: string;
+  columns: {
+    title: string;
+    key: string;
+  }[];
+  dataSource: {}[];
+};
+
+export default function ExcelDownloadButton({
+  title,
+  columns,
+  dataSource
+}: ExcelDownloadButtonProps) {
+  const { year, month, day, hours, minutes, seconds } = getDateTimeStrings();
+  const fileName = `[핔]${title.replace(" ", "")}_${year.substring(
+    2
+  )}${month}${day}_${hours}${minutes}${seconds}`;
+
+  const getExcelColumns = columns =>
+    columns.map((item, index) => (
+      <ExcelColumn key={index} label={item.title} value={item.key} />
+    ));
+
+  return (
+    <ExcelFile
+      filename={fileName}
+      element={<Button icon="file-excel">엑셀 다운</Button>}
+    >
+      <ExcelSheet data={dataSource} name={title}>
+        {getExcelColumns(columns)}
+      </ExcelSheet>
+    </ExcelFile>
+  );
+}
