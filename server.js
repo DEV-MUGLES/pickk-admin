839cb54b7bcd9946790dbbf580b24545f01c966b
Choi Sumin <greatSumini@gmail.com>
2020-01-06 20:24:09 +0900

    [Feature] add gzip on server

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
 require("dotenv").config();
-const express = require("express");
-const next = require("next");
-
 const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT;
+
+const express = require("express");
+const { join } = require("path");
+const { parse } = require("url");
+
+const next = require("next");
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app
-  .prepare()
-  .then(() => {
-    const server = express();
+const compression = require("compression");
 
-    server.get("*", (req, res) => {
-      return handle(req, res);
-    });
+app.prepare().then(() => {
+  const server = express();
+  server.use(compression());
 
-    server.listen(3000, err => {
-      if (err) throw err;
-      console.log("> Ready on http://localhost:3000");
-    });
-  })
-  .catch(ex => {
-    console.error(ex.stack);
-    process.exit(1);
+  server.get("*", (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl;
+
+    if (pathname === "/service-worker.js") {
+      const filePath = join(__dirname, ".next", pathname);
+      app.serveStatic(req, res, filePath);
+      return;
+    }
+
+    return handle(req, res);
   });
+
+  server.listen(port, err => {
+    if (err) throw err;
+
+    console.log(
+      `> Ready on http://localhost:${port} : ${dev ? "dev" : "prod"}`
+    );
+  });
+});
