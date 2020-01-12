61efc22a59ff0acd76b4421d869d0701608f1a4d
gywls517 <gywls517@naver.com>
2020-01-12 18:54:01 +0900

    add AdminLogoDefaultIcon temporarily

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import Colors from "@src/components/atoms/colors";
+import LogoDefaultIcon from "@src/components/atoms/logo/default";
+import Space from "@src/components/atoms/space";
 
 export default function Login() {
-  return <div>로그인을 하란 말이야 그랭</div>;
+  return (
+  <Layout>
+    <Space level={10} direction="COL" />
+    <LogoDefaultIcon
+            style={{ width: "80px", height: "48px" }}
+            fill={Colors.Black}
+    />
+    <div>pickk admin</div>
+    <Space level={10} direction="COL" />
+
+  </Layout>
+  );
 }
+
+const Layout = styled.body`
+  width:fit-content;
+  margin:0 auto;
+
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+
+  background-color: ${Colors.LightGrey};;
+`
---
+++ src/components/organisms/LoginForm.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import Colors from "@src/components/atoms/colors";
+import LogoDefaultIcon from "@src/components/atoms/logo/default";
+import Space from "@src/components/atoms/space";
+
+export default function LoginForm() {
+  return (
+  <Wrapper>
+      <div>안냥안냥안냥</div>
+  </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width:fit-content;
+  
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+
+  background-color: ${Colors.White};
+  padding:10px;
+`
