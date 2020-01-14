f1a836a2ff8ab5d46c03be072319842b3529cf4b
gywls517 <gywls517@naver.com>
2020-01-14 16:15:38 +0900

    add board filter button area

---
+++ src/components/organisms/Board/Filter/ButtonArea.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Button} from 'antd';
+import styled from 'styled-components';
+
+import Colors from '@src/components/atoms/colors';
+import Space from '@src/components/atoms/space';
+
+export type BoardFilterButtonAreaProps = {
+    handleSubmit: () => void;
+    handleReset: () => void
+};
+
+export default function ButtonArea(
+    {
+        handleSubmit,
+        handleReset,
+    }: BoardFilterButtonAreaProps,
+) {
+  return (
+    <Wrapper>
+        <SearchButton icon="search" type="primary" onClick={handleSubmit}>검색</SearchButton>
+        <Space direction="ROW" />
+        <ResetButton icon="reload" onClick={handleReset}>초기화</ResetButton>
+    </Wrapper>
+   );
+}
+
+const Wrapper = styled.header`
+    width: fit-content;
+    display: flex;
+    flex-direction: row;
+    align-items:center;
+`;
+
+const SearchButton = styled(Button)`
+    width: 100px;
+`;
+
+const ResetButton = styled(Button)`
+    width: 100px;
+    color: ${Colors.Primary};
+    border-color: ${Colors.Primary}
+`;
