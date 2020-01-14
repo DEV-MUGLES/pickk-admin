c843faf97fe6db5a800eae99202878a4c7c8762c
gywls517 <47051596+gywls517@users.noreply.github.com>
2020-01-14 16:49:26 +0900

    [Feature] Add boardFilterButtonArea (#13)
    * add board filter button area
    Resolve #10

---
+++ src/components/organisms/Board/Filter/ButtonArea.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Button} from 'antd';
+import styled from 'styled-components';
+
+import Colors from '@src/components/atoms/colors';
+import Space from '@src/components/atoms/space';
+
+export type BoardFilterButtonAreaProps = {
+    handleSubmit: () => void;
+    handleReset: () => void
+};
+
+export default function ButtonArea(
+    {
+        handleSubmit,
+        handleReset,
+    }: BoardFilterButtonAreaProps,
+) {
+  return (
+    <Wrapper>
+        <SubmitButton icon="search" type="primary" onClick={handleSubmit}>조회</SubmitButton>
+        <Space direction="ROW" />
+        <ResetButton icon="undo" onClick={handleReset}>초기화</ResetButton>
+    </Wrapper>
+   );
+}
+
+const Wrapper = styled.div`
+    width: fit-content;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+const SubmitButton = styled(Button)`
+    width: 100px;
+`;
+
+const ResetButton = styled(SubmitButton)`
+    color: ${Colors.Primary};
+    border-color: ${Colors.Primary}
+`;
