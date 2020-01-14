d58295551b97bac3fd9d20b1bbeae0c5b18ebd69
gywls517 <gywls517@naver.com>
2020-01-14 13:25:10 +0900

    add board-filter-header

--- src/components/organisms/Board/Filter/Header.tsx
+++ src/components/organisms/Board/Filter/Header.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import {Typography, Tooltip, Button} from 'antd';
+import styled from 'styled-components';
 
-export default function LoginForm() {
+export type BoardFilterHeaderProps = {
+    title: string;
+    guideText?: string
+};
+
+export default function Header(
+    {
+        title,
+        guideText,
+    }: BoardFilterHeaderProps,
+) {
   return (
-    <></>
-  );
+    <Wrapper>
+        <StyledTypographyText strong>
+            {title}
+        </StyledTypographyText>
+        <Tooltip placement="bottomRight" title={guideText}>
+            <Button icon="info-circle" />
+        </Tooltip>
+    </Wrapper>
+   );
 }
+
+const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items:center;
+`;
+
+const StyledTypographyText = styled(Typography.Text)`
+    margin-right:auto;
+`;
