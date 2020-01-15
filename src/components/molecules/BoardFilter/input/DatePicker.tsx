accec067156f9f66b7775ee5283d110edbcc649a
gywls517 <gywls517@naver.com>
2020-01-15 12:06:10 +0900

    initial commit

---
+++ src/components/molecules/BoardFilter/input/DatePicker.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// export type BoardFilterHeaderProps = {
+//     title: string;
+//     guideText?: string
+// };
+
+export default function Header(
+    // {
+    //     title,
+    //     guideText,
+    // }: BoardFilterHeaderProps,
+) {
+  return (
+    <></>
+   );
+}
