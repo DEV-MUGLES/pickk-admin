dfe60b18d8195ed1fd2c03652878a40955a2b3e7
Choi Sumin <greatSumini@gmail.com>
2020-01-16 17:01:07 +0900

    [Enhance] add withBoardFilterContext to Board

--- src/components/templates/Board.tsx
+++ src/components/templates/Board.tsx
@@ -1,23 +1,41 @@
-import Space from '../atoms/space';
+import {useState} from 'react';
 import styled from 'styled-components';
 
+import {withBoardFilterContext} from '@src/contexts/BoardFilter';
+
 import Header, {BoardHeaderProps} from '../organisms/Board/Header';
+import Filter, {BoardFilterProps} from '../organisms/Board/Filter';
 import Table, {BoardTableProps} from '../organisms/Board/Table';
+import Space from '../atoms/space';
 
-export type BoardProps = BoardHeaderProps & BoardTableProps;
+export type BoardProps = BoardHeaderProps &
+  Omit<BoardFilterProps, 'onSubmit'> &
+  BoardTableProps;
 
-export default function Board(props: BoardProps) {
+function Board(props: BoardProps) {
   const headerProps: BoardHeaderProps = props;
+  const filterProps: Omit<BoardFilterProps, 'onSubmit'> = props;
   const tableProps: BoardTableProps = props;
+
+  const [filterParameter, setFilterParameter] = useState({});
+
+  const handleFilterSubmit = filterForm => {
+    setFilterParameter(filterForm);
+  };
 
   return (
     <Wrapper>
       <Header {...headerProps} />
       <Space level={2} />
+      <Filter {...filterProps} onSubmit={handleFilterSubmit} />
+      <Space level={2} />
       <Table {...tableProps} />
+      <Space level={2} />
     </Wrapper>
   );
 }
+
+export default withBoardFilterContext(Board);
 
 const Wrapper = styled.div`
   width: 100%;
