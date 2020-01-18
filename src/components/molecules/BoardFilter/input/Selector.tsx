ab735ecfe19c3360c58a9d3f36e5ec51841b95ab
gywls517 <gywls517@naver.com>
2020-01-18 22:43:34 +0900

    modify optional props to nonoptional

--- src/components/molecules/BoardFilter/input/Selector.tsx
+++ src/components/molecules/BoardFilter/input/Selector.tsx
@@ -8,7 +8,7 @@ const {Option} = Select;
 
 export type SelectorProps = {
   name: string;
-  select?: [{name: string; value: string}];
+  select: [{name: string; value: string}];
 };
 
 export default function Selector({
