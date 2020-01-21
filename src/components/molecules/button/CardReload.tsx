c2e02685b642400e9c3b2ffcfe36bdc39dbcef90
gywls517 <gywls517@naver.com>
2020-01-21 16:33:57 +0900

    move time label from header to reload button

--- src/components/molecules/button/CardReload.tsx
+++ src/components/molecules/button/CardReload.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import {Button} from 'antd';
+import {Typography, Button} from 'antd';
+import styled from 'styled-components';
+
+import { getDateTimeStrings } from '@src/lib/DateParser';
+import Space from '@src/components/atoms/space';
 
 export type CardReloadButtonProps = {
+  time: string;
   onClick: () => void;
 };
 
 export default function CardReloadButton({
-  onClick,
+    time,
+    onClick,
 }: CardReloadButtonProps) {
+    const {hours, minutes} = getDateTimeStrings(time);
     return (
-    <Button shape="circle" icon="reload" size="small" onClick={onClick} />
+        <Wrapper>
+            <StyledText>최근</StyledText>
+            <Space direction="ROW"/>
+            <Typography.Text>{hours}:{minutes}</Typography.Text>
+            <Space direction="ROW" size={8}/>
+            <Button shape="circle" icon="reload" size="small" onClick={onClick} />
+        </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+    display:flex;
+    flex-direction:row;
+    align-items:center;
+    width:fit-content;
+`;
+
+const StyledText = styled(Typography.Text)`
+    font-size:11px;
+`;
--- src/components/organisms/Dashboard/Card/Header.tsx
+++ src/components/organisms/Dashboard/Card/Header.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import {Typography, Button} from 'antd';
+import {Typography} from 'antd';
 import styled from 'styled-components';
 
-import { getDateTimeStrings } from '@src/lib/DateParser';
 import CardReloadButton from '@src/components/molecules/button/CardReload';
-import Space from '@src/components/atoms/space';
 
 export type DashboardCardHeaderProps = {
   title: string;
@@ -17,18 +15,10 @@ export default function DashboardCardHeader({
   time,
   onClick,
 }: DashboardCardHeaderProps) {
-    const {hours, minutes} = getDateTimeStrings(time);
-
     return (
     <Wrapper>
         <Typography.Text strong>{title}</Typography.Text>
-        <ReloadWrapper>
-            <StyledText>최근</StyledText>
-            <Space direction="ROW"/>
-            <Typography.Text>{hours}:{minutes}</Typography.Text>
-            <Space direction="ROW" size={8}/>
-            <CardReloadButton onClick={onClick} />
-        </ReloadWrapper>
+        <CardReloadButton time={time} onClick={onClick} />
     </Wrapper>
   );
 }
@@ -40,15 +30,4 @@ const Wrapper = styled.header`
   align-items: center;
   width: 100%;
   height: fit-content;
-`;
-
-const ReloadWrapper = styled.div`
-    display:flex;
-    flex-direction:row;
-    align-items:center;
-    width:fit-content;
-`;
-
-const StyledText = styled(Typography.Text)`
-    font-size:11px;
 `;
