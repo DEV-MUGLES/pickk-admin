831eb5381f851ac9174aa117926a18d54945c626
gywls517 <gywls517@naver.com>
2020-02-04 17:08:02 +0900

    changed column and dataSource

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,10 +1,9 @@
-import React, {useState} from 'react';
-import { Input, Typography, DatePicker, Button, Modal } from 'antd';
-import styled from 'styled-components';
+import React from 'react';
 import MainLayout from '@src/components/templates/MainLayout';
 import Board from '@src/components/templates/Board';
 
 import {BoardFilterProps} from '@src/components/organisms/Board/Filter';
+import DiscountSection from '@src/components/organisms/Board/Table/DiscountSection';
 import Datepicker from '@src/components/molecules/BoardFilter/input/DatePicker';
 import SelectInput from '@src/components/molecules/BoardFilter/input/SelectInput';
 import Selector from '@src/components/molecules/BoardFilter/input/Selector';
@@ -12,49 +11,8 @@ import InputBox from '@src/components/molecules/BoardFilter/input/InputBox';
 import MultiChecker from '@src/components/molecules/BoardFilter/input/MultiChecker';
 import ItemCategorySelector from '@src/components/molecules/BoardFilter/input/ItemCategorySelector';
 import moment from 'moment';
-import Space from '@src/components/atoms/space';
-import Colors from '@src/components/atoms/colors';
-
-const { Text } = Typography;
-const {RangePicker} = DatePicker;
-const { Search } = Input;
 
 export default function Products() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const modalOpen = () => {
-    setIsModalOpen(true);
-  };
-  const modalClose = e => {
-    setIsModalOpen(false);
-  };
-  const handleSubmit = e => {
-    setIsLoading(true);
-  };
-
-  const addModal = () => (
-          <Modal
-            visible={isModalOpen}
-            title="Title"
-            onOk={handleSubmit}
-            onCancel={modalClose}
-            footer={[
-              <Button key="back" onClick={modalClose}>
-                Return
-              </Button>,
-              <Button key="submit" type="primary" loading={isLoading} onClick={handleSubmit}>
-                Submit
-              </Button>,
-            ]}
-          >
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-          </Modal>
-      );
-
   const columns = [
     {
       title: '카테고리',
@@ -70,24 +28,6 @@ export default function Products() {
       sorter: (a, b) => a.name > b.name,
     },
     {
-      title: '구독할인율',
-      dataIndex: 'subscribeDiscountRate',
-      key: 'subscribeDiscountRate',
-      sorter: (a, b) => a.subscribeDiscountRate - b.subscribeDiscountRate,
-    },
-    {
-      title: '구독할인기간',
-      dataIndex: 'subscribeDiscountPeriod',
-      key: 'subscribeDiscountPeriod',
-      sorter: (a, b) => a.subscribeDiscountPeriod > b.subscribeDiscountPeriod,
-    },
-    {
-      title: '상품번호',
-      dataIndex: 'id',
-      key: 'id',
-      sorter: (a, b) => a.id > b.id,
-    },
-    {
       title: '정가',
       dataIndex: 'originalPrice',
       key: 'originalPrice',
@@ -98,6 +38,12 @@ export default function Products() {
       dataIndex: 'salePrice',
       key: 'salePrice',
       sorter: (a, b) => a.salePrice - b.salePrice,
+    },
+    {
+      title: '구독할인율',
+      dataIndex: 'subscribeDiscountRate',
+      key: 'subscribeDiscountRate',
+      sorter: (a, b) => a.subscribeDiscountRate - b.subscribeDiscountRate,
     },
     {
       title: '리뷰수',
@@ -116,6 +62,12 @@ export default function Products() {
       dataIndex: 'salesCount',
       key: 'salesCount',
       sorter: (a, b) => a.salesCount - b.salesCount,
+    },
+    {
+      title: '상품번호',
+      dataIndex: 'id',
+      key: 'id',
+      sorter: (a, b) => a.id > b.id,
     },
     {
       title: '색상',
@@ -137,14 +89,6 @@ export default function Products() {
     },
   ];
 
-  const colorList = ['화이트', '그레이', '블랙'];
-  let colorListString = '';
-  colorListString += colorList.map(item => item);
-
-  const sizeList = ['XS', 'S', 'M', 'L', 'XL'];
-  let sizeListString = '';
-  sizeListString += sizeList.map(item => item);
-
   const dataSource = [];
   for (let i = 1; i < 92; ++i) {
     dataSource.push({
@@ -153,164 +97,41 @@ export default function Products() {
       itemMinorType: '맨투맨',
       name: '기모 짱짱 맨투맨 (그레이)' + i,
       subscribeDiscountRate: 5 % i,
-      subscribeDiscountPeriod: `2020/01/10-2020/02/0${i % 9 + 1}`,
+      subscribeDiscountStartPeriod: `2020-01-1${(i % 9) + 1}`,
+      subscribeDiscountEndPeriod: `2020-02-0${(i % 9) + 1}`,
       originalPrice: 39000 + i,
       salePrice: 19900 - i,
       reviewCount: i,
       totalViewCount: 200 - i,
       salesCount: 92 - i,
-      colors: colorListString,
-      sizes: sizeListString,
+      colors: '화이트',
+      sizes: 'L',
       options: '',
+      influencerDiscountData: [
+        {
+          key: '스타일리스트1',
+          name: '스타일리스트1',
+          subscribeDiscountRate: 5,
+          subscribeDiscountStartPeriod: `2020-01-11`,
+          subscribeDiscountEndPeriod: `2020-02-04`,
+        },
+        {
+          key: '스타일리스트2',
+          name: '스타일리스트2',
+          subscribeDiscountRate: 3,
+          subscribeDiscountStartPeriod: `2020-01-01`,
+          subscribeDiscountEndPeriod: `2020-02-07`,
+        },
+        {
+          key: '스타일리스트3',
+          name: '스타일리스트3',
+          subscribeDiscountRate: 4,
+          subscribeDiscountStartPeriod: `2020-01-18`,
+          subscribeDiscountEndPeriod: `2020-02-02`,
+        },
+      ],
     });
   }
-
-  let influencerData = [
-    {
-      key: 1,
-      name: '박효진박효진',
-      subscriberNumber: '12만',
-    },
-    {
-      key: 2,
-      name: '최수민최수민',
-      subscriberNumber: '33만',
-    },
-    {
-      key: 3,
-      name: '깡깡깡스스스',
-      subscriberNumber: '19만',
-    },
-  ];
-  for (let i = 1; i < 92; ++i) {
-    dataSource.push({
-      key: i,
-      id: 'id',
-      itemMinorType: '맨투맨',
-      name: '기모 짱짱 맨투맨 (그레이)' + i,
-      subscribeDiscountRate: 5 % i,
-      subscribeDiscountPeriod: `2020/01/10-2020/02/0${i % 9 + 1}`,
-      originalPrice: 39000 + i,
-      salePrice: 19900 - i,
-      reviewCount: i,
-      totalViewCount: 200 - i,
-      salesCount: 92 - i,
-      colors: colorListString,
-      sizes: sizeListString,
-      options: '',
-    });
-  }
-
-  const expandedRowRender = record => {
-
-    const influencerSaleData = [];
-    for (let i = 1; i < 5; ++i) {
-      influencerSaleData.push({
-        key: i,
-        name: `스타일리스트${i}`,
-        subscribeDiscountRate: 5 % i,
-        subscribeDiscountPeriod: `2020/01/10-2020/02/0${i % 9 + 1}`,
-      });
-    }
-
-    return(
-    <ExpandedRowWrapper>
-      <Space/>
-        <Row>
-            <Text style={{color: "black"}}>구독 할인</Text>
-            <Space direction="ROW" level={4}/>
-            <Input size="small" value={record.subscribeDiscountRate} style={{width: "40px"}} />
-            <Space direction="ROW"/>
-            <span> %</span>
-            <Space direction="ROW" level={2}/>
-            <RangePicker
-              name="choicedSelectValue"
-              size="small"
-              style={{width: "250px"}}
-              value={[
-                moment(record.subscribeDiscountPeriod.substring(0, 10)),
-                moment(record.subscribeDiscountPeriod.substring(11, 21)),
-              ]}
-            />
-            <Space direction="ROW" level={2}/>
-            <Button size="small">변경</Button>
-            <Space direction="ROW"/>
-            <Button size="small" style={{color: Colors.Primary}}>초기화</Button>
-        </Row>
-        <Space level={2}/>
-        <Text style={{color: "black"}}>인플루언서 할인</Text>
-        <Space level={1}/>
-        {influencerSaleData.map((item, index) => (
-        <>
-            <Row>
-                    <Text>{index + 1}</Text>
-                    <Space direction="ROW" level={2}/>
-                    <Text>{item.name}</Text>
-                    <Space direction="ROW" level={2}/>
-                    <Input size="small" value={item.subscribeDiscountRate} style={{width: "40px"}} />
-                    <Space direction="ROW"/>
-                    <span> %</span>
-                    <Space direction="ROW" level={2}/>
-                    <RangePicker
-                      name="choicedSelectValue"
-                      size="small"
-                      style={{width: "250px"}}
-                      value={[
-                        moment(item.subscribeDiscountPeriod.substring(0, 10)),
-                        moment(item.subscribeDiscountPeriod.substring(11, 21)),
-                      ]}
-                    />
-                    <Space direction="ROW" level={2}/>
-                    <Button size="small">변경</Button>
-                    <Space direction="ROW"/>
-                    <Button size="small" style={{color: "#f33"}}>삭제</Button>
-            </Row>
-            <Space/>
-        </>
-        ))}
-        <Space/>
-        <Button type="primary" onClick={modalOpen} size="small" block>
-            추가
-        </Button>
-        <Modal
-            visible={isModalOpen}
-            title="인플루언서 찾기"
-            onOk={handleSubmit}
-            onCancel={modalClose}
-            footer={[
-              <Button key="back" onClick={modalClose}>
-                닫기
-              </Button>,
-              <Button key="submit" type="primary" loading={isLoading} onClick={handleSubmit}>
-                다음
-              </Button>,
-            ]}
-          >
-          <Search placeholder="이름으로 검색" onSearch={value => {
-            influencerData = influencerData.filter(data => (data.name.includes(value)));
-            }} enterButton />
-          <Space level={2}/>
-          <SearchResultWrapper>
-          {influencerData.map((item, index) => (
-            <>
-                <Button block>
-                  <Row>
-                        <Text>{index + 1}</Text>
-                        <Space direction="ROW" level={12}/>
-                        <Text style={{color: "black"}}>{item.name}</Text>
-                        <Space direction="ROW" level={2}/>
-                        <Text>구독자 수 : {item.subscriberNumber}명</Text>
-                        <Space direction="ROW" level={2}/>
-                    </Row>
-                </Button>
-                <Space level={1}/>
-            </>
-            ))}
-            </SearchResultWrapper>
-          </Modal>
-
-    </ExpandedRowWrapper>
-    ); };
 
   const actions = [
     {
@@ -339,15 +160,19 @@ export default function Products() {
       name: 'period',
       defaultValue: {
         type: 'all',
-        startDate: moment().subtract(1, 'months').format('YYYY-MM-DD'),
+        startDate: moment()
+          .subtract(1, 'months')
+          .format('YYYY-MM-DD'),
         endDate: moment().format('YYYY-MM-DD'),
       },
       labelText: '조회기간',
       guideText: '조회기간 부가 설명입니다',
-      select: [{name: '전체', value: 'all'},
-      {name: '상품등록일', value: 'registerProductDate'},
-      {name: '판매시작일', value: 'startSellingDate'},
-      {name: '판매종료일', value: 'endSellingDate'}],
+      select: [
+        {name: '전체', value: 'all'},
+        {name: '상품등록일', value: 'registerProductDate'},
+        {name: '판매시작일', value: 'startSellingDate'},
+        {name: '판매종료일', value: 'endSellingDate'},
+      ],
       Component: Datepicker,
     },
     {
@@ -358,7 +183,8 @@ export default function Products() {
       },
       labelText: '상세조건',
       guideText: '상세조건 부가 설명입니다',
-      select: [{name: '전체', value: 'all'},
+      select: [
+        {name: '전체', value: 'all'},
         {name: '수취인명', value: 'receiverName'},
         {name: '구매자명', value: 'buyerName'},
         {name: '구매자연락처', value: 'buyerPhoneNum'},
@@ -366,7 +192,8 @@ export default function Products() {
         {name: '주문번호', value: 'orderNum'},
         {name: '상품주문번호', value: 'productOrderNum'},
         {name: '상품번호', value: 'productNum'},
-        {name: '송장번호', value: 'invoiceNum'}],
+        {name: '송장번호', value: 'invoiceNum'},
+      ],
       Component: SelectInput,
     },
     {
@@ -377,7 +204,8 @@ export default function Products() {
       },
       labelText: '주문상태',
       guideText: '주문상태 부가 설명입니다',
-      select: [{name: '전체', value: 'all'},
+      select: [
+        {name: '전체', value: 'all'},
         {name: '신규주문', value: 'newOrder'},
         {name: '발주확인', value: 'confirmedOrder'},
         {name: '발주확인해제', value: 'cancelConfirmedOrder'},
@@ -397,11 +225,20 @@ export default function Products() {
     {
       name: 'sellingState',
       defaultValue: {
-        choices: ['판매대기', '판매중', '품절', '승인대기', '판매중지', '판매종료', '판매금지'],
+        choices: [
+          '판매대기',
+          '판매중',
+          '품절',
+          '승인대기',
+          '판매중지',
+          '판매종료',
+          '판매금지',
+        ],
       },
       labelText: '판매상태',
       guideText: '판매상태 부가 설명입니다',
-      options: [{name: '판매대기', value: 'waitSelling'},
+      options: [
+        {name: '판매대기', value: 'waitSelling'},
         {name: '판매중', value: 'nowSelling'},
         {name: '품절', value: 'soldOut'},
         {name: '승인대기', value: 'waitConfirm'},
@@ -432,27 +269,10 @@ export default function Products() {
           '상단에 판매상태별 상품 건수 확인이 가능하며, 건수를 클릭 시 상품목록에 등록한 상품이 조회됩니다.',
           `특정 상품의 조회를 원하신다면, "검색어" 기능을 사용해서 상품번호/상품명/제조사명 등 원하시는 유형으로 조회하실 수 있습니다.`,
         ]}
-        {...{columns, dataSource, expandedRowRender, actions}}
+        {...{columns, dataSource, actions}}
+        expandedRowRender={DiscountSection}
         filter={{title: '상세 조회하기', guideText: '', inputs}}
       />
     </MainLayout>
   );
 }
-
-const ExpandedRowWrapper = styled.div`
-    display:flex;
-    flex-direction:column;
-    width: 800px;
-`;
-
-const Row = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-`;
-
-const SearchResultWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items:center;
-`;
