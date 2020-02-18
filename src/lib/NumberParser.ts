332994f7f7e0eebf981a0e842666a94277bd214b
gywls517 <gywls517@naver.com>
2020-02-18 17:15:15 +0900

    add addCommaToNumber function in NumberParser.ts

--- src/lib/NumberParser.ts
+++ src/lib/NumberParser.ts
@@ -5,3 +5,20 @@ export const addLeadingZeros = (num: number, length: number) => {
   }
   return new Array(length - result.length).fill('0').toString() + result;
 };
+
+export const addCommaToNumber = (num: number) => {
+  const numString = num.toString();
+  let result = '';
+  let i = 0;
+  for (; i < numString.length - 1; i++) {
+    result += numString[i];
+    if ((numString.length - i - 1) % 3 === 0) {
+      result += ',';
+    }
+  }
+  result += numString[i];
+  if (result === '0') {
+    return null;
+  }
+  return result;
+};
