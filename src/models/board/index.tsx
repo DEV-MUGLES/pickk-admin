d59133646f4c33c93249ce2c58b9e4a104fbc986
gywls517 <gywls517@naver.com>
2020-02-20 14:38:26 +0900

    add board model

---
+++ src/models/board/index.tsx
@@ -0,0 +1,48 @@
+import {BoardFilterProps} from '@src/components/organisms/Board/Filter';
+import ProductsBoardModel from './products';
+import {useAxios} from '@src/lib/services/Api';
+
+export type actionsType = Array<{
+  icon?: string;
+  text: string;
+  // tslint:disable-next-line: no-any
+  onClick: (num: any) => void;
+}>;
+
+export type columnsType = Array<{
+  title: string;
+  dataIndex: string;
+  key: string;
+  // tslint:disable-next-line: no-any
+  render?: (text: any) => JSX.Element;
+  // tslint:disable-next-line: no-any
+  sorter: (a: any, b: any) => any;
+  width: number;
+  ellipsis: boolean;
+}>;
+
+export type inputsType = BoardFilterProps['inputs'];
+
+// tslint:disable-next-line: interface-name
+export interface BoardModelType {
+  actions: actionsType;
+  columns: columnsType;
+  inputs: inputsType;
+}
+
+export class BoardModel implements BoardModelType {
+  actions: actionsType;
+  columns: columnsType;
+  inputs: inputsType;
+
+  useBoardModelData = (path, variables) =>
+    useAxios('GET', `/${path}`, {variables});
+}
+
+export const getBoardModelByName = (name: string) => {
+  let boardModel;
+  if (name === 'products') {
+    boardModel = new ProductsBoardModel();
+  }
+  return boardModel;
+};
