f8f485704cd317f1b2819c5a4cf62f6a61cf68c4
gywls517 <gywls517@naver.com>
2020-02-20 14:43:12 +0900

    use product board model by give getBoardModelByName the name of the page given by props

--- src/components/templates/Board.tsx
+++ src/components/templates/Board.tsx
@@ -1,21 +1,44 @@
 import {useState} from 'react';
 import styled from 'styled-components';
 
+import {getBoardModelByName} from '@src/models/board';
+import {addCommaToNumber} from '@src/lib/NumberParser';
 import {withBoardFilterContext} from '@src/contexts/BoardFilter';
-
 import Header, {BoardHeaderProps} from '../organisms/Board/Header';
 import Filter, {BoardFilterProps} from '../organisms/Board/Filter';
 import Table, {BoardTableProps} from '../organisms/Board/Table';
 import Space from '../atoms/space';
 
-export type BoardProps = BoardHeaderProps &
-  BoardTableProps & {
-    filter?: Omit<BoardFilterProps, 'onSubmit'>;
-  };
+export type BoardProps = BoardHeaderProps & {
+  name: string;
+  filter?: Omit<BoardFilterProps, 'onSubmit' | 'inputs'>;
+  // tslint:disable-next-line: no-any
+};
 
 function Board(props: BoardProps) {
+  const boardModel = getBoardModelByName(props.name);
+  const {columns, actions, inputs} = boardModel;
+
+  const {loading, error, data} = boardModel.useBoardModelData('items', {
+    brand: 1,
+  });
+
+  let dataSource = [];
+  if (data) {
+    dataSource = data.map(item => {
+      return {
+        ...item,
+        itemMinorType: '니트',
+        originalPrice: addCommaToNumber(item.originalPrice) + '원',
+        salePrice: addCommaToNumber(item.salePrice) + '원',
+        reviewCount: 5,
+        totalViewCount: 5,
+        salesCount: 5,
+      };
+    });
+  }
+
   const headerProps: BoardHeaderProps = props;
-  const tableProps: BoardTableProps = props;
 
   const [filterParameter, setFilterParameter] = useState({});
 
@@ -25,14 +48,23 @@ function Board(props: BoardProps) {
 
   const filterProps: BoardFilterProps = {
     ...props.filter,
+    inputs,
     onSubmit: handleFilterSubmit,
+  };
+
+  const tableProps: BoardTableProps = {
+    title: props.title,
+    columns,
+    dataSource,
+    actions,
+    loading,
   };
 
   return (
     <Wrapper>
       <Header {...headerProps} />
       <Space level={2} />
-      <Filter {...filterProps} onSubmit={handleFilterSubmit} />
+      <Filter {...filterProps} />
       <Space level={2} />
       <Table {...tableProps} />
       <Space level={2} />
