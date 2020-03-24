72146705e1ebac40e4e6ca322e4c3f5b619dc787
Choi Sumin <greatSumini@gmail.com>
2020-03-25 01:36:26 +0900

    [Feature] add stock,products service (#39)
    * [Feature] add ItemService.manageStock, getProductList
    
    * [Feature] add ProductService.setStock
    resolve #39

--- src/lib/services/Item.ts
+++ src/lib/services/Item.ts
@@ -1,6 +1,7 @@
 import base from './Api';
 import {Filter} from '@src/types';
 import {Item} from '@src/types/Item';
+import {Product} from '@src/types/Product';
 
 const getItemList = async (
   filter: Filter,
@@ -9,17 +10,27 @@ const getItemList = async (
   next: string;
   previous: string;
   results: Item[];
-}> => {
-  console.log(filter);
-  return base(true)
+}> =>
+  base(true)
     .get(`/partner/items/`, {
       params: {...filter, isReviewd: true, limit: 150, offset: 0},
     })
     .then(res => res.data);
-};
+
+const manageStock = (on: boolean, ids: number[]) =>
+  base(true)
+    .post('/partner/items/manage_stock/', {on, ids})
+    .then(res => res.data);
+
+const getProductList = (id: number): Promise<{results: Product[]}> =>
+  base(true)
+    .get(`/partner/items/${id}/products`)
+    .then(res => res.data);
 
 const ItemService = {
   getItemList,
+  manageStock,
+  getProductList,
 };
 
 export default ItemService;
