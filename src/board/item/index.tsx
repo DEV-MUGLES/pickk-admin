a0ecbba0ee57c3719f4d829faa318b4ce6633ce2
greatSumini <greatsumini@gmail.com>
2020-03-25 16:33:26 +0900

    [Feature] add ItemTable:stock column

--- src/board/item/index.tsx
+++ src/board/item/index.tsx
@@ -1,3 +1,5 @@
+import {Typography, Button} from 'antd';
+
 import Filter from '@src/components/organisms/Board/Filter';
 import Table from '@src/components/organisms/Board/Table';
 import Space from '@src/components/atoms/space';
@@ -9,15 +11,38 @@ import {itemInputs} from './inputs';
 import {itemColumns} from './table/columns';
 import {BoardProps} from '../props';
 
+const {Text} = Typography;
+
 function ItemBoard({title}: BoardProps) {
+  const newItemColumns = [
+    ...itemColumns,
+    {
+      title: '재고관리',
+      dataIndex: 'isStockManaged',
+      key: 'isStockManaged',
+      sorter: (a, b) => a.isStockManaged > b.isStockManaged,
+      width: 60,
+      render: value => {
+        if (value) {
+          return <Text type="secondary">OFF</Text>;
+        }
+        return <Button size="small">재고관리</Button>;
+      },
+    },
+  ];
+
   return (
     <>
       <Filter title={title} inputs={itemInputs} />
       <Space level={2} />
-      <Table title={title} columns={itemColumns} />
+      <Table title={title} columns={newItemColumns} />
       <Space level={2} />
     </>
   );
 }
 
-export default withBoardContext(ItemBoard, {name: ''}, useItemTable);
+export default withBoardContext(
+  ItemBoard,
+  {name: null, isReviewed: false},
+  useItemTable,
+);
