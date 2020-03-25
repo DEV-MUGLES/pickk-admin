ab66b7c90ed13b99c149c81a83337a2308b9cea1
Yang <ybh1760@gmail.com>
2020-03-26 03:03:29 +0900

    [Enhance] refactor placement/preview

--- src/board/placement/preview/index.tsx
+++ src/board/placement/preview/index.tsx
@@ -1,30 +1,49 @@
-import React from 'react';
-import {Typography} from 'antd';
-import {usePlacementPreview} from '@src/hooks';
-import {FilterWrapper} from '@src/components/organisms/Board/Filter';
+import React, {useState} from 'react';
+import styled from 'styled-components';
 
-const {Text} = Typography;
+import {usePlacementPreview} from '@src/hooks';
+
+import Preview from '@src/components/molecules/button/preview';
+import Colors from '@src/components/atoms/colors';
 
 const LABEL_TEXTS = [
-  '발송 전 취소 요청',
-  '발송 전 배송지 변경',
-  '신규 주문',
-  '발주 확인 완료',
+  {label: '발송 전 취소 요청', iconType: 'close', filter: {name: 'cancel'}},
+  {
+    label: '발송 전 배송지 변경',
+    iconType: 'environment',
+    filter: {name: 'exchange'},
+  },
+  {label: '신규 주문', iconType: 'shopping', filter: {name: 'newOrder'}},
+  {label: '발주 확인 완료', iconType: 'check', filter: {name: 'confirmed'}},
 ];
 
 export default function PlacementPreview() {
   const {data: previewData} = usePlacementPreview([]);
+  const [selectedPreview, setSelectedPreview] = useState(null);
 
   if (previewData) {
     return (
-      <FilterWrapper>
-        {Object.values(previewData).map((v, i) => (
-          <div>
-            <Text key={LABEL_TEXTS[i]}>{`${LABEL_TEXTS[i]} : ${v}`}</Text>
-          </div>
+      <PreviewWrapper>
+        {Object.values(previewData).map((value, index) => (
+          <Preview
+            key={LABEL_TEXTS[index].label}
+            {...LABEL_TEXTS[index]}
+            count={value}
+            {...{index, selectedPreview, setSelectedPreview}}
+          />
         ))}
-      </FilterWrapper>
+      </PreviewWrapper>
     );
   }
   return <></>;
 }
+
+const PreviewWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  height: fit-content;
+  background-color: ${Colors.White};
+  padding: 16px 6rem;
+`;
