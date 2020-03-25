61c65b33935dffbf418c01f243e9479b03dd9e49
greatSumini <greatsumini@gmail.com>
2020-03-26 05:34:56 +0900

    [Feature] add usePlacementTable hook

--- src/hooks/table/Item.ts
+++ src/hooks/table/Item.ts
@@ -2,4 +2,4 @@ import {useAxiosQuery} from '../Api';
 import ItemService from '@src/lib/services/Item';
 import {Item} from '@src/types';
 
-export const useItemTable = useAxiosQuery<Item>(ItemService.getItemList);
+export const useItemTable = useAxiosQuery<Item[]>(ItemService.getList);
