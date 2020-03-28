8a015943f4da90ff01379a1bbfba9c671b71a40d
gywls517 <gywls517@naver.com>
2020-03-28 23:17:57 +0900

    [Enhance] Remove stockOn, stockOff action from itemActions

--- src/board/item/table/actions.ts
+++ src/board/item/table/actions.ts
@@ -1,12 +1,4 @@
-import ItemService from '@src/lib/services/Item';
-
 export const itemActions = [
-  {
-    text: '재고관리 OFF',
-    onClick: async (ids: number[]) => {
-      await ItemService.manageStock(false, ids);
-    },
-  },
   /*{
     text: '구독 할인 설정',
     onClick: (nums: number[]) => {
