e9319371b3b6ebc92bf574445b19096f4e2c7a54
gywls517 <gywls517@naver.com>
2020-03-28 16:50:49 +0900

    [Enhance] remove stockManageOn action from itemActions

--- src/board/item/table/actions.ts
+++ src/board/item/table/actions.ts
@@ -2,12 +2,6 @@ import ItemService from '@src/lib/services/Item';
 
 export const itemActions = [
   {
-    text: '재고관리 ON',
-    onClick: async (ids: number[]) => {
-      await ItemService.manageStock(true, ids);
-    },
-  },
-  {
     text: '재고관리 OFF',
     onClick: async (ids: number[]) => {
       await ItemService.manageStock(false, ids);
