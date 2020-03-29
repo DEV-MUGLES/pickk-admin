d176ad8b3fec79cfda4b2d785c687bf6d5090930
양병훈 <ybh1760@gmail.com>
2020-03-29 21:44:36 +0900

    [Enhance] change status text (#72)
    주문상태 text 변경

--- src/board/placement/inputs.ts
+++ src/board/placement/inputs.ts
@@ -26,9 +26,9 @@ export const itemInputs = [
     labelText: '주문상태',
     select: [
       {name: '전체', value: null},
-      {name: '발주대기', value: 'PAID'},
-      {name: '발송대기', value: 'PLACED'},
-      {name: '배송중', value: 'SHIPPING'},
+      {name: '결제완료', value: 'PAID'},
+      {name: '발주완료', value: 'PLACED'},
+      {name: '발송완료', value: 'SHIPPING'},
       {name: '배송완료', value: 'DELIVERED'},
     ],
     Component: Selector,
