bcb101a7eef1741b41709c19457c5bf60902d0cd
Yang <ybh1760@gmail.com>
2020-03-30 02:28:35 +0900

    [Enhance] refactor stringSorter

--- src/lib/sorter.ts
+++ src/lib/sorter.ts
@@ -1,8 +1,11 @@
 export const stringSorter = (a, b) => {
   if (a && b) {
     return a.localeCompare(b);
-  } else if (a || b) {
+  } else if (a) {
     return -1;
+  } else if (b) {
+    return 1;
+  } else {
+    return 0;
   }
-  return 1;
 };
