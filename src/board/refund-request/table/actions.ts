73f0050c84144211c9982952117d2f99f75a68e7
greatSumini <greatsumini@gmail.com>
2020-04-03 21:47:14 +0900

    [Feature] add refundRequestActions

--- src/board/refund-request/table/actions.ts
+++ src/board/refund-request/table/actions.ts
@@ -1,8 +1,12 @@
-export const itemActions = [
-  /*{
-    text: '구독 할인 설정',
-    onClick: (nums: number[]) => {
-      return;
-    },
-  },*/
+import RefundRequestService from '@src/lib/services/RefundRequest';
+
+export const refundRequestActions = [
+  {
+    text: '수거 완료',
+    onClick: (nums: number[]) => RefundRequestService.pick(nums),
+  },
+  {
+    text: '반품 완료',
+    onClick: (nums: number[]) => RefundRequestService.confirm(nums),
+  },
 ];
