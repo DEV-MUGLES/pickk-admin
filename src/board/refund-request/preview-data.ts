2bd4a9565cae9ad9f10829c3167232bfabae35bf
greatSumini <greatsumini@gmail.com>
2020-04-03 19:45:15 +0900

    [Feature] add RefundRequest:preview-data

---
+++ src/board/refund-request/preview-data.ts
@@ -0,0 +1,17 @@
+export const refundRequestPreviewData = [
+  {
+    label: '반품 요청',
+    iconType: 'shopping',
+    filterValue: {status: 'REFUND_REQUESTED'},
+  },
+  {
+    label: '반품 수거 완료',
+    iconType: 'check',
+    filterValue: {status: 'PICKED'},
+  },
+  {
+    label: '반품 완료',
+    iconType: 'shopping',
+    filterValue: {status: 'REFUND_CONFIRMED'},
+  },
+];
