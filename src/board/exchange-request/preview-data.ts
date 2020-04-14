e5d67e17003b5087f8a80d758b631d5e8fec4be7
greatSumini <greatsumini@gmail.com>
2020-04-15 00:58:47 +0900

    [Enhance] add ExchangeRequestPreview:RESHIPPING,CANCELLED,REJECTED resolve #91

--- src/board/exchange-request/preview-data.ts
+++ src/board/exchange-request/preview-data.ts
@@ -1,22 +1,39 @@
 export const exchangeRequestPreviewData = [
   {
     label: '교환 요청',
-    iconType: 'shopping',
+    iconType: 'unordered-list',
     filterValue: {status: 'REQUESTED'},
   },
   {
     label: '교환 수거 중',
-    iconType: 'check',
+    iconType: 'loading',
     filterValue: {status: 'PICKING'},
   },
   {
     label: '교환 수거 완료',
-    iconType: 'check',
+    iconType: 'import',
     filterValue: {status: 'PICKED'},
+  },
+  {
+    label: '교환 배송 중',
+    iconType: 'car',
+    filterValue: {status: 'RESHIPPING'},
   },
   {
     label: '교환 배송 완료',
     iconType: 'shopping',
     filterValue: {status: 'REDELIVERED'},
+  },
+  {
+    label: '교환 취소',
+    iconType: 'check',
+    filterValue: {status: 'CANCELLED'},
+    disabled: true,
+  },
+  {
+    label: '교환 거부',
+    iconType: 'shopping',
+    filterValue: {status: 'REJECTED'},
+    disabled: true,
   },
 ];
