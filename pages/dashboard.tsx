778d602ec10751a207a8163b44e3aeee1dc7aadb
greatSumini <greatsumini@gmail.com>
2020-04-21 19:32:01 +0900

    [Chore] fix dashboard text

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import MainLayout from '@src/components/templates/MainLayout';
 
 export default function Dashboard() {
-  return <MainLayout>
-    zzzzzz
-  </MainLayout>;
+  return (
+    <MainLayout>
+      대시보드 준비중입니다. 좌측 메뉴 페이지를 이동해주세요.
+    </MainLayout>
+  );
 }
 
 Dashboard.getInitialProps = () => {
