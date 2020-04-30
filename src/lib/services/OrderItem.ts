38e27884c2a381d7bfbe8900a7d13d57ff57c1ea
greatSumini <greatsumini@gmail.com>
2020-04-30 23:31:03 +0900

    [Feature] add OrderItem:refundRequest

--- src/lib/services/OrderItem.ts
+++ src/lib/services/OrderItem.ts
@@ -6,7 +6,7 @@ import {Filter, OrderItemShip, OrderItem} from '@src/types';
 const ship = async (ships: OrderItemShip[]) =>
   base(true)
     .post(`/partner/order_items/ship/`, ships)
-    .then(res => {
+    .then((res) => {
       const {requested, success} = res.data;
       if (requested === 0) {
         message.error(`변경된 데이터가 없습니다.`);
@@ -20,7 +20,7 @@ const ship = async (ships: OrderItemShip[]) =>
         );
       }
     })
-    .catch(err => {
+    .catch((err) => {
       message.error('실패했습니다. - ' + err);
     });
 
@@ -29,11 +29,37 @@ const getList = (filter: Filter): Promise<OrderItem[]> =>
     .get('/partner/order_items/', {
       params: filter,
     })
-    .then(res => res.data);
+    .then((res) => res.data);
+
+const refundRequest = (id: number, reason: string) =>
+  base(true)
+    .post(`/partner/order_items/${id}/refund_request/`, {reason})
+    .then(() => {
+      message.success(`반품으로 전환되었습니다.`);
+    })
+    .catch((err) => {
+      switch (err?.response?.status) {
+        case 400:
+          alert(`${err.response.data?.errorMessage}\n다시 확인해주세요.`);
+          break;
+        case 404:
+          alert(`해당 주문을 찾을 수 없습니다.\n다시 확인해주세요.`);
+          break;
+        default:
+          alert(
+            `${
+              err.response.data?.errorMessage || '반품으로 변경에 실패했습니다.'
+            }\n운영팀으로 문의 주시면 신속히 처리해드리겠습니다.\n이용에 불편을 드려 죄송합니다.`,
+          );
+          break;
+      }
+      throw err;
+    });
 
 const OrderItemService = {
   ship,
   getList,
+  refundRequest,
 };
 
 export default OrderItemService;
