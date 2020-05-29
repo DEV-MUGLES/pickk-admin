b9d9a0c101e75035761822f288048ad2af8acd6f
greatSumini <greatsumini@gmail.com>
2020-05-29 16:57:00 +0900

    [Enhance] add ExchangeRequest.switchToRefundRequest confirm

---
+++ src/board/exchange-request/table/actions.tsx
@@ -0,0 +1,38 @@
+import {message, Modal, Icon} from 'antd';
+
+import ExchangeRequestService from '@src/lib/services/ExchangeRequest';
+
+const {confirm} = Modal;
+
+export const exchangeRequestActions = [
+  {
+    text: '반품으로 변경',
+    onClick: async (ids: number[]) => {
+      if (ids.length !== 1) {
+        message.warning(
+          '일괄 변경은 지원하지 않습니다.\n1개의 주문건만 선택해주세요.',
+        );
+        return Promise.resolve(false);
+      }
+      try {
+        confirm({
+          title: '반품 변경 확인',
+          icon: <Icon type="ExclamationCircleOutlined" />,
+          content: '정말로 반품으로 변경하시겠습니까?.',
+          okText: '예',
+          okType: 'danger',
+          cancelText: '아니오',
+          async onOk() {
+            await ExchangeRequestService.switchToRefundRequest(ids[0]);
+          },
+          onCancel() {
+            message.warning('취소되었습니다.');
+          },
+        });
+        return Promise.resolve(true);
+      } catch {
+        return Promise.resolve(false);
+      }
+    },
+  },
+];
