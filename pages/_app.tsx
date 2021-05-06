3f5d5e6017a6f6ce365cef255aaa2de96bc9e7d1
greatSumini <greatsumini@gmail.com>
2021-05-06 13:50:35 +0900

    feat: :sparkles: add apolloClient module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,21 @@ import Head from 'next/head';
 import {BackTop} from 'antd';
 import 'antd/dist/antd.css';
 
+import {ApolloProvider} from '@apollo/client';
+import {useApollo} from '@src/lib/apollo';
+
 function PickkAdminApp({Component, pageProps}: AppProps) {
+  const apolloClient = useApollo(pageProps);
+
   return (
-    <>
+    <ApolloProvider client={apolloClient}>
       <Head>
         <title>핔 스토어 어드민</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
       <BackTop />
-    </>
+    </ApolloProvider>
   );
 }
 
---
+++ src/lib/apollo.ts
@@ -0,0 +1,84 @@
+import {useMemo} from 'react';
+import {ApolloClient, ApolloLink, from, InMemoryCache} from '@apollo/client';
+import {createUploadLink} from 'apollo-upload-client';
+import merge from 'deepmerge';
+import isEqual from 'lodash/isEqual';
+
+import {getCookie} from './utils';
+
+export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__';
+
+let apolloClient: ApolloClient<unknown>;
+
+if (!process.env.NEW_API_URL) {
+  throw new Error('env.NEW_API_URL not found!');
+}
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = getCookie('accessToken');
+  operation.setContext({
+    headers: {
+      Authorization: token ? `Bearer ${token}` : null,
+    },
+  });
+
+  return forward(operation);
+});
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: from([
+      authMiddleware,
+      createUploadLink({
+        uri: process.env.NEW_API_URL,
+      }),
+    ]),
+    cache: new InMemoryCache(),
+  });
+}
+
+export function initializeApollo(initialState = null) {
+  const _apolloClient = apolloClient ?? createApolloClient();
+
+  // If your page has Next.js data fetching methods that use Apollo Client, the initial state
+  // gets hydrated here
+  if (initialState) {
+    // Get existing cache, loaded during client side data fetching
+    const existingCache = _apolloClient.extract();
+
+    // Merge the existing cache into data passed from getStaticProps/getServerSideProps
+    const data = merge(initialState, existingCache, {
+      // combine arrays using object equality (like in sets)
+      arrayMerge: (destinationArray, sourceArray) => [
+        ...sourceArray,
+        ...destinationArray.filter((d) =>
+          sourceArray.every((s) => !isEqual(d, s)),
+        ),
+      ],
+    });
+
+    // Restore the cache with the merged data
+    _apolloClient.cache.restore(data);
+  }
+  // For SSG and SSR always create a new Apollo Client
+  if (typeof window === 'undefined') return _apolloClient;
+  // Create the Apollo Client once in the client
+  if (!apolloClient) apolloClient = _apolloClient;
+
+  return _apolloClient;
+}
+
+export function addApolloState(client, pageProps) {
+  if (pageProps?.props) {
+    pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract();
+  }
+
+  return pageProps;
+}
+
+export function useApollo(pageProps: Record<string, unknown>) {
+  const state = pageProps[APOLLO_STATE_PROP_NAME];
+  const store = useMemo(() => initializeApollo(state), [state]);
+  return store;
+}
