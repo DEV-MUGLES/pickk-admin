a7d4e716e0693613582a36ad382b31e6f0065eb9
greatSumini <greatsumini@gmail.com>
2021-05-06 14:47:25 +0900

    feat: :sparkles: add HomePage server-side auth flow
    - 토큰 유효성 검사를 해 통과하면 dashboard, 아니면 login으로 redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
-
 import {GetServerSideProps} from 'next';
+import {ME_SELLER_QUERY} from '@pickk/common';
 
-export default function Home() {
+import {initializeApollo} from '@src/lib/apollo';
+import {getCookie} from '@src/lib/utils';
+
+export default function HomePage() {
   return <div>You can't see this page</div>;
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: {
-      destination: '/dashboard',
-      permanent: false,
-    },
-  };
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
+  try {
+    const token = getCookie('accessToken', req);
+
+    if (!token) {
+      throw new Error('no token');
+    }
+
+    await initializeApollo().query({
+      query: ME_SELLER_QUERY,
+      context: {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    });
+
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      },
+    };
+  } catch (error) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
 };
