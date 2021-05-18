812703ef42541324767bb8aebc8bc487e3e44d1a
syoung125 <laura120555@gmail.com>
2021-05-18 13:22:01 +0900

    feat: :sparkles: add item table action

--- src/component/board/item/table/actions.ts
+++ src/component/board/item/table/actions.ts
@@ -1,24 +1,52 @@
+import {message, Modal} from 'antd';
+
+import {useBulkUpdateItems} from '@pickk/common';
+
+const {confirm} = Modal;
+
+const handleSetIsSellable =
+  (isSellable: boolean) => async (ids: number[], mutate) => {
+    try {
+      confirm({
+        title: `상품 ${isSellable ? '' : '비'}활성화 확인`,
+        content: `상품을 ${isSellable ? '' : '비'}활성화 하시겠습니까?`,
+        okText: '예',
+        okType: 'danger',
+        cancelText: '아니오',
+        async onOk() {
+          await mutate({
+            variables: {
+              bulkUpdateItemInput: {
+                isSellable,
+              },
+              ids,
+            },
+          });
+        },
+        onCancel() {
+          message.warning('취소되었습니다.');
+        },
+      });
+
+      return Promise.resolve(true);
+    } catch {
+      return Promise.resolve(false);
+    }
+  };
+
 export const itemActions = [
   {
     text: '상품 활성화',
-    onClick: async () => {
-      return Promise.resolve(false);
-    },
+    onClick: handleSetIsSellable(true),
+    hook: useBulkUpdateItems,
   },
   {
     text: '상품 비활성화',
-    onClick: async (ids: number[]) => {
-      return Promise.resolve(false);
-    },
+    onClick: handleSetIsSellable(false),
+    hook: useBulkUpdateItems,
   },
   {
     text: '상품 삭제',
-    onClick: async (ids: number[]) => {
-      return Promise.resolve(false);
-    },
-  },
-  {
-    text: '전체 업데이트',
     onClick: async (ids: number[]) => {
       return Promise.resolve(false);
     },
