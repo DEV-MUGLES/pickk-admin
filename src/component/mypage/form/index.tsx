dbe8859d4d1e3149a239d16be41b103221ed2866
syoung125 <laura120555@gmail.com>
2021-06-01 17:36:01 +0900

    feat: :fire: remove ReturnAddressEditForm

--- src/component/mypage/form/index.tsx
+++ src/component/mypage/form/index.tsx
@@ -1,4 +1,5 @@
 export {default as BaseInfoEditForm} from './base-info-edit';
+export {default as ServiceCenterInfoEditForm} from './service-center-info-edit';
 export {default as ShippingPolicyEditForm} from './shipping-policy-edit';
 export {default as ClaimPolicyEditForm} from './claim-policy-edit';
-export {default as ReturnAddressEditForm} from './return-address-edit';
+export {default as SettlePolicyEditForm} from './settle-policy-edit';
