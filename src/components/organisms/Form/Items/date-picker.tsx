af821e48d0de4792d57b5716e8992e5692fff15b
syoung125 <laura120555@gmail.com>
2021-06-02 18:16:07 +0900

    refactor: :label: assign type to DatePickerFormItem

--- src/components/organisms/Form/Items/date-picker.tsx
+++ src/components/organisms/Form/Items/date-picker.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import dayjs from 'dayjs';
+import dayjs, {Dayjs} from 'dayjs';
 
 import DayjsDatePicker from '../../../molecules/BoardFilter/input/DayjsDatePicker';
 
+export type DatePickerFormItemProps = {
+  defaultValue: Dayjs;
+  onChange: (value: Dayjs) => void;
+  value: Dayjs;
+};
+
 function DatePickerFormItem({
   defaultValue,
-  onChange = (value: any) => null,
+  onChange = (value: Dayjs) => null,
   value,
-}) {
+}: DatePickerFormItemProps) {
   return (
     <DayjsDatePicker
       defaultValue={value ? dayjs(defaultValue) : null}
