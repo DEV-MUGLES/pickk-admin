30d153b352f13513e945a0a0e55b240bf37a7359
syoung125 <laura120555@gmail.com>
2021-06-02 11:42:23 +0900

    fix: :bug: fix InputNumber

--- src/components/organisms/Form/Items/input-number.tsx
+++ src/components/organisms/Form/Items/input-number.tsx
@@ -1,26 +1,24 @@
 import {Input, InputProps} from 'antd';
-import {ChangeEvent, useEffect, useState} from 'react';
 
 export type InputNumberProps = InputProps;
 
 function InputNumber(props: InputNumberProps) {
-  const {onChange} = props;
-
-  const triggerChange = (changedValue: ChangeEvent<HTMLInputElement>) => {
-    onChange?.(changedValue);
-  };
+  const {value, onChange} = props;
 
   const onNumberChange = (e) => {
-    const newValue = parseInt(e.target.value || '0', 10);
+    const newValue = parseInt(e.target.value || '0');
 
     if (Number.isNaN(newValue)) {
       return;
     }
 
-    triggerChange({...e, target: {...e.target, value: newValue}});
+    onChange?.({
+      ...e,
+      target: {...e.target, name: e.target.name, value: newValue},
+    });
   };
 
-  return <Input {...props} onChange={onNumberChange} />;
+  return <Input {...props} value={value || '0'} onChange={onNumberChange} />;
 }
 
 export default InputNumber;
