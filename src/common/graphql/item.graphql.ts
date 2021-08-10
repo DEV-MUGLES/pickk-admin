7e3278b3058fec370952f8dce1279cf669c9c9ff
syoung125 <laura120555@gmail.com>
2021-08-10 14:52:18 +0900

    refactor: :recycle: merge item graphql query, fragment

--- src/common/graphql/item.graphql.ts
+++ src/common/graphql/item.graphql.ts
@@ -65,3 +65,52 @@ export const PRODUCT_FRAG = gql`
     updatedAt
   }
 `;
+
+export const ITEMS_QUERY = gql`
+  query Items($itemFilter: ItemFilter, $pageInput: PageInput) {
+    items(itemFilter: $itemFilter, pageInput: $pageInput) {
+      id
+      imageUrl
+      majorCategory {
+        id
+        name
+      }
+      minorCategory {
+        id
+        name
+      }
+      name
+      originalPrice
+      sellPrice
+      finalPrice
+      prices {
+        ...ItemPriceFrag
+      }
+      products {
+        ...ProductFrag
+      }
+      isInfiniteStock
+      isSoldout
+      notice {
+        id
+        type
+        message
+        startAt
+        endAt
+      }
+      options {
+        ...ItemOptionFrag
+      }
+      isMdRecommended
+      isSellable
+      urls {
+        isPrimary
+        url
+      }
+      createdAt
+    }
+  }
+  ${ITEM_PRICE_FRAG}
+  ${PRODUCT_FRAG}
+  ${ITEM_OPTION_FARG}
+`;
