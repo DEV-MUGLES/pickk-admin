b9bdab4103a4d81932b94f17c99f1f425caf7ee4
syoung125 <laura120555@gmail.com>
2021-08-11 14:13:57 +0900

    feat: update orderItems, placements colums

--- src/components/placement/table/columns.tsx
+++ src/components/placement/table/columns.tsx
@@ -4,7 +4,9 @@ import {orderItemColumns} from '@src/components/order-items/table';
 import {stringSorter} from '@src/common/helpers/sorter';
 
 export const placementColumns = [
-  ...orderItemColumns,
+  ...orderItemColumns.slice(0, 2), // 상품주문번호, 주문번호
+  orderItemColumns[3], // 주문상태
+  orderItemColumns[2], // 주문일시
   {
     title: '택배사',
     dataIndex: 'courierId',
@@ -22,13 +24,6 @@ export const placementColumns = [
     ellipsis: true,
   },
   {
-    title: '배송지',
-    dataIndex: '',
-    key: '',
-    width: 120,
-    ellipsis: true,
-  },
-  {
     title: '배송추적',
     dataIndex: 'trackingViewUrl',
     key: 'trackingViewUrl',
@@ -41,10 +36,76 @@ export const placementColumns = [
     sorter: (a, b) => b.trackingViewUrl - a.trackingViewUrl,
     width: 90,
   },
+  ...orderItemColumns.slice(5, 10), // 상품명, 옵션, 수량, 구매자명, 구매자 연락처
+  {
+    title: '구매자 이메일',
+    dataIndex: 'orderBuyerEmail',
+    key: 'orderBuyerEmail',
+    sorter: (a, b) => stringSorter(b.orderBuyerEmail, a.orderBuyerEmail),
+    width: 100,
+    ellipsis: true,
+  },
+  ...orderItemColumns.slice(10),
+  {
+    title: '수취인 연락처',
+    dataIndex: 'orderReceiverPhoneNumber',
+    key: 'orderReceiverPhoneNumber',
+    sorter: (a, b) =>
+      stringSorter(b.orderReceiverPhoneNumber, a.orderReceiverPhoneNumber),
+    width: 100,
+    ellipsis: true,
+  },
   {
     title: '우편번호',
-    dataIndex: '',
-    key: '',
+    dataIndex: 'orderReceiverPostalCode',
+    key: 'orderReceiverPostalCode',
+    sorter: (a, b) =>
+      stringSorter(b.orderReceiverPostalCode, a.orderReceiverPostalCode),
+    width: 100,
+    ellipsis: true,
+  },
+  {
+    title: '배송지 주소',
+    dataIndex: 'orderReceiverBaseAddress',
+    key: 'orderReceiverBaseAddress',
+    sorter: (a, b) =>
+      stringSorter(b.orderReceiverBaseAddress, a.orderReceiverBaseAddress),
+    width: 100,
+    ellipsis: true,
+  },
+  {
+    title: '배송지 상세주소',
+    dataIndex: 'orderReceiverDetailAddress',
+    key: 'orderReceiverDetailAddress',
+    sorter: (a, b) =>
+      stringSorter(b.orderReceiverDetailAddress, a.orderReceiverDetailAddress),
+    width: 100,
+    ellipsis: true,
+  },
+  {
+    title: '배송지 주소 (통합)',
+    dataIndex: 'orderReceiverFullAddress',
+    key: 'orderReceiverFullAddress',
+    sorter: (a, b) =>
+      stringSorter(b.orderReceiverFullAddress, a.orderReceiverFullAddress),
+    width: 120,
+    ellipsis: true,
+  },
+  // @ TODO 배송메세지
+  {
+    title: '판매가격',
+    dataIndex: 'itemFinalPrice',
+    key: 'itemFinalPrice',
+    sorter: (a, b) => stringSorter(b.itemFinalPrice, a.itemFinalPrice),
+    width: 120,
+    ellipsis: true,
+  },
+  {
+    title: '리뷰어',
+    dataIndex: 'recommenderNickname',
+    key: 'recommenderNickname',
+    sorter: (a, b) =>
+      stringSorter(b.recommenderNickname, a.recommenderNickname),
     width: 120,
     ellipsis: true,
   },
