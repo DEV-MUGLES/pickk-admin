7fb216abb5dc0a6fee4d5234c7f23e2c6b8c0e93
syoung125 <laura120555@gmail.com>
2021-08-11 14:11:53 +0900

    feat: add mapRecord prop to BoardContext dataFetchConfig

--- src/types/Board.ts
+++ src/types/Board.ts
@@ -1,10 +1,1 @@
-import {QueryResult} from '@apollo/client';
-
-export type DataFetchConfig = {
-  useBoardData: (options?) => QueryResult;
-  operationName: string;
-  filterName: string;
-  defaultFilter?: Filter;
-};
-
 export type Filter = Record<string, any>;
