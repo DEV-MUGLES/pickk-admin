c73c4e1be07675e65d3690bcffda90dca5f8fbe1
syoung125 <laura120555@gmail.com>
2021-08-13 09:47:13 +0900

    chore: sort shipmentsColumns ascend by shippingAt

--- src/components/shipments/table/columns.tsx
+++ src/components/shipments/table/columns.tsx
@@ -1,17 +1,30 @@
+import {ColumnsType} from 'antd/lib/table';
+
 import {getTimeString, stringSorter} from '@src/common/helpers';
 
 import {placementColumns} from '@src/components/placement/table';
 
-export const shipmentsColumns = [
-  ...placementColumns.slice(0, 3),
+export const shipmentsColumns: ColumnsType<any> = [
+  ...placementColumns.slice(0, 2),
+  {
+    title: '주문 일시',
+    dataIndex: 'paidAt',
+    key: 'paidAt',
+    render: (value) => <div>{getTimeString(value)}</div>,
+    sorter: (a, b) => stringSorter(b.paidAt, a.paidAt),
+    width: 100,
+    ellipsis: true,
+  },
   {
     title: '발송일시',
     dataIndex: 'shippingAt',
     key: 'shippingAt',
     render: (value) => <div>{getTimeString(value)}</div>,
     sorter: (a, b) => stringSorter(b.shippingAt, a.shippingAt),
+    defaultSortOrder: 'ascend',
     width: 100,
     ellipsis: true,
   },
-  ...placementColumns.slice(3),
+  placementColumns[2],
+  ...placementColumns.slice(4),
 ];
