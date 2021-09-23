109109a378b384e620cbb29205cf1191ca934256
syoung125 <laura120555@gmail.com>
2021-09-23 18:14:33 +0900

    fix: update wrong shipmentsColumns order

--- src/components/shipments/table/columns.tsx
+++ src/components/shipments/table/columns.tsx
@@ -5,16 +5,7 @@ import {getTimeString, stringSorter} from '@src/common/helpers';
 import {placementColumns} from '@src/components/placements/table';
 
 export const shipmentsColumns: ColumnsType<any> = [
-  ...placementColumns.slice(0, 2),
-  {
-    title: '주문 일시',
-    dataIndex: 'paidAt',
-    key: 'paidAt',
-    render: (value) => <div>{getTimeString(value)}</div>,
-    sorter: (a, b) => stringSorter(b.paidAt, a.paidAt),
-    width: 100,
-    ellipsis: true,
-  },
+  ...placementColumns.slice(0, 3),
   {
     title: '발송일시',
     dataIndex: 'shippingAt',
@@ -25,6 +16,5 @@ export const shipmentsColumns: ColumnsType<any> = [
     width: 100,
     ellipsis: true,
   },
-  placementColumns[2],
-  ...placementColumns.slice(4),
+  ...placementColumns.slice(3),
 ];
