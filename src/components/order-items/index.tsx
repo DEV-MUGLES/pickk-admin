d7352908d0484db1ad57e680f3827ccd4613117f
syoung125 <laura120555@gmail.com>
2021-09-23 15:04:19 +0900

    chore: remove unusing code

--- src/components/order-items/index.tsx
+++ src/components/order-items/index.tsx
@@ -2,15 +2,12 @@ import Header from '../common/organisms/Board/Header';
 import Filter from '../common/organisms/Board/Filter';
 import Table from '../common/organisms/Board/Table';
 
-import {useBoardContext} from '@src/common/contexts/Board';
 import {BoardProps} from '../props';
 
 import {orderItemInputs} from './inputs';
 import {orderItemColumns} from './table';
 
 function OrderItemBoard(props: BoardProps) {
-  const {state} = useBoardContext();
-
   return (
     <>
       <Header {...props} />
