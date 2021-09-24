0ae14d610c9b3ab9300716a4f6dba6edff8e7f0b
syoung125 <laura120555@gmail.com>
2021-09-24 20:19:14 +0900

    feat: :sparkles: add InquiriesBoardContainer

---
+++ src/containers/inquiries/inquiries.tsx
@@ -0,0 +1,24 @@
+import BoardStoreProvider from '@src/common/contexts/Board';
+import {InquiriesTable} from '@src/components/inquiries';
+
+import {useInquiries} from './hooks';
+
+type InquiriesBoardContainerProps = {
+  title: string;
+  subTitle: string;
+};
+
+export default function InquiriesBoardContainer(
+  props: InquiriesBoardContainerProps,
+) {
+  return (
+    <BoardStoreProvider
+      dataFetchConfig={{
+        useBoardData: useInquiries,
+        operationName: 'meSellerInquiries',
+        filterName: 'filter',
+      }}>
+      <InquiriesTable {...props} />
+    </BoardStoreProvider>
+  );
+}
