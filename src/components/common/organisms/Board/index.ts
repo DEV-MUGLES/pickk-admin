a925ef4b696351cbeaad8a67c8e4d10bd9edb220
syoung125 <laura120555@gmail.com>
2021-09-24 20:16:31 +0900

    chore: add organisms/Board index.ts, organisms index.ts

---
+++ src/components/common/organisms/Board/index.ts
@@ -0,0 +1,4 @@
+export {default as BoardHeader} from './Header';
+export {default as Preview} from './preview';
+export {default as BoardFilter} from './Filter';
+export {default as BoardTable} from './Table';
