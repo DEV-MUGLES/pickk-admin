5cc3282cce090b29551c77a6c4e424944a44db1b
syoung125 <laura120555@gmail.com>
2021-09-24 20:58:46 +0900

    feat: :sparkles: add useInquiriesPreview hook

---
+++ src/components/inquiries/preview/hooks/use-inquiries-preview.ts
@@ -0,0 +1,24 @@
+import {gql, useQuery} from '@apollo/client';
+import {Query, QueryMeSellerInquiriesCountArgs} from '@pickk/common';
+
+const GET_ME_SELLER_INQUIRIES_COUNT = gql`
+  query meSellerInquiriesCount($forceUpdate: Boolean) {
+    meSellerInquiriesCount(forceUpdate: $forceUpdate) {
+      id
+      delayed
+      not_answered
+    }
+  }
+`;
+
+export const useInquiriesPreview = () => {
+  const {data, refetch} = useQuery<
+    Pick<Query, 'meSellerInquiriesCount'>,
+    QueryMeSellerInquiriesCountArgs
+  >(GET_ME_SELLER_INQUIRIES_COUNT);
+
+  return {
+    data: data?.meSellerInquiriesCount ?? {},
+    reload: () => refetch({forceUpdate: true}),
+  };
+};
