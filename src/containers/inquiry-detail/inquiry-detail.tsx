a902fd47722fb8b495e29e61f76d8245a0d71653
syoung125 <laura120555@gmail.com>
2021-09-27 01:47:02 +0900

    feat: :sparkles: add InquiryDetailContainer

---
+++ src/containers/inquiry-detail/inquiry-detail.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+import {
+  InquiryDetailItemSection,
+  InquiryDetailOrderSection,
+  InquiryDetailContentSection,
+  InquiryDetailAnswerSection,
+} from '@src/components/inquiry-detail';
+
+import {useInquiry} from './hooks';
+
+import {WHITE} from '@src/common/constants/colors';
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledSection = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  padding: 1.2rem 1.6rem;
+  margin-bottom: 0.8rem;
+
+  background-color: ${WHITE};
+`;
+
+export type InquiryDetailContainerProps = {
+  id: number;
+};
+
+export default function InquiryDetailContainer({
+  id,
+}: InquiryDetailContainerProps) {
+  const {data, loading} = useInquiry(id);
+
+  if (!data) {
+    return null;
+  }
+
+  if (!data && !loading) {
+    return <StyledSection>문의가 없습니다.</StyledSection>;
+  }
+
+  // @TODO 해당 셀러의 브랜드에 대한 문의인지 확인
+
+  const {item, orderItemMerchantUid, orderItem} = data;
+
+  return (
+    <StyledWrapper>
+      <StyledSection>
+        <InquiryDetailItemSection {...item} />
+        {!!orderItemMerchantUid && <InquiryDetailOrderSection {...orderItem} />}
+      </StyledSection>
+      <StyledSection>
+        <InquiryDetailContentSection {...data} />
+        <InquiryDetailAnswerSection id={id} />
+      </StyledSection>
+    </StyledWrapper>
+  );
+}
