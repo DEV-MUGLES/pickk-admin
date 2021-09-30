f8e24aedcb3c8c2d5b5ba2cbbc27e74d0d468ad5
syoung125 <laura120555@gmail.com>
2021-10-01 04:10:23 +0900

    feat: :sparkles: add getRefundRequestStatusStatusDisplayName

--- src/common/helpers/alias/index.ts
+++ src/common/helpers/alias/index.ts
@@ -2,3 +2,4 @@ export * from './bankName.alias';
 export * from './inquiry-type.alias';
 export * from './orderItemStatus.alias';
 export * from './orderItemClaimStatus.alias';
+export * from './refund-request-status.alias';
