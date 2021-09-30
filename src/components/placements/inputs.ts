29bea96f25423d570a8fb99feb911ed222b9b361
syoung125 <laura120555@gmail.com>
2021-10-01 01:46:26 +0900

    chore: :bulb: comment placementInputs

--- src/components/placements/inputs.ts
+++ src/components/placements/inputs.ts
@@ -5,9 +5,9 @@ import CheckBox from '../common/molecules/BoardFilter/input/CheckBox';
 export const placementInputs = [
   ...orderItemInputs, // 검색창, 조회기간
   // @TODO 발송지연 안내여부
-  {
-    name: '',
-    labelText: '발송지연 안내여부',
-    Component: CheckBox,
-  },
+  // {
+  //   name: '',
+  //   labelText: '발송지연 안내여부',
+  //   Component: CheckBox,
+  // },
 ];
