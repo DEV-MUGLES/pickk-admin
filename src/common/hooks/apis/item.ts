52ac7acb8acc50dce1f3ff32cdea62ee6d5df84a
syoung125 <laura120555@gmail.com>
2021-10-07 16:36:37 +0900

    chore: :fire: remove useUpdateItem

--- src/common/hooks/apis/item.ts
+++ src/common/hooks/apis/item.ts
@@ -1,53 +1,5 @@
-import {ApolloCache, gql, useMutation} from '@apollo/client';
-import {
-  Mutation,
-  MutationBulkUpdateItemsArgs,
-  MutationUpdateItemArgs,
-  UpdateItemInput,
-} from '@pickk/common';
-
-export const useUpdateItem = () => {
-  const [updateItem] = useMutation<unknown, MutationUpdateItemArgs>(gql`
-    mutation UpdateItem($itemId: Int!, $updateItemInput: UpdateItemInput!) {
-      updateItem(itemId: $itemId, updateItemInput: $updateItemInput) {
-        id
-        name
-        imageUrl
-        description
-        isInfiniteStock
-        majorCategoryId
-        majorCategory {
-          id
-          name
-        }
-        minorCategoryId
-        minorCategory {
-          id
-          name
-        }
-      }
-    }
-  `);
-
-  const updateCategoryCache =
-    (itemId: number, updateItemInput: UpdateItemInput) =>
-    (cache: ApolloCache<unknown>) => {
-      const {majorCategoryId, minorCategoryId} = updateItemInput;
-      cache.modify({
-        id: `Item:${itemId}`,
-        fields: {
-          majorCategory: () => ({
-            __ref: `ItemCategory:${majorCategoryId}`,
-          }),
-          minorCategory: () => ({
-            __ref: `ItemCategory:${minorCategoryId}`,
-          }),
-        },
-      });
-    };
-
-  return {updateItem, updateCategoryCache};
-};
+import {gql, useMutation} from '@apollo/client';
+import {Mutation, MutationBulkUpdateItemsArgs} from '@pickk/common';
 
 export const useBulkUpdateItems = () =>
   useMutation<
