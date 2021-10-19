7ad2c796e8046b14824e835fab3eab35299050a4
syoung125 <laura120555@gmail.com>
2021-10-19 16:12:19 +0900

    refactor: :recycle: refactor CancelRequestsBoard by applying new structure

--- pages/request/cancel.tsx
+++ pages/request/cancel.tsx
@@ -1,4 +1,4 @@
-import CancelRequestsBoardContainer from '@src/containers/cancel-requests';
+import {CancelRequestsBoardContainer} from '@src/containers/cancel-requests';
 
 export default function CancelRequests() {
   return <CancelRequestsBoardContainer />;
---
+++ src/containers/cancel-requests/hooks/use-cancel-requests.ts
@@ -0,0 +1,49 @@
+import {useQuery} from '@apollo/client';
+import {
+  QueryMeSellerOrderItemsArgs,
+  OrderItemFilter,
+  OrderItemClaimStatus,
+} from '@pickk/common';
+
+import {BoardDataFetcher} from '@components/new-common/template/board';
+import {useOrderItemsCount} from '@containers/order-items/hooks';
+
+import {
+  GET_ORDER_ITEMS,
+  OrderItemDataType,
+} from '@containers/order-items/hooks';
+
+export const GET_CANCEL_REQUESTS = GET_ORDER_ITEMS;
+
+export type CancelRequestDataType = OrderItemDataType;
+
+const {Cancelled, CancelRequested} = OrderItemClaimStatus;
+
+export const useCancelRequests: BoardDataFetcher<
+  CancelRequestDataType,
+  OrderItemFilter
+> = ({filter, pageInput}) => {
+  const orderItemFilter: OrderItemFilter = {
+    ...filter,
+    claimStatusIn: [Cancelled, CancelRequested],
+  };
+
+  const {data, loading, refetch} = useQuery<
+    {meSellerOrderItems: CancelRequestDataType[]},
+    QueryMeSellerOrderItemsArgs
+  >(GET_CANCEL_REQUESTS, {
+    variables: {
+      orderItemFilter,
+      pageInput,
+    },
+  });
+
+  const total = useOrderItemsCount({filter: orderItemFilter});
+
+  return {
+    data: data?.meSellerOrderItems,
+    total,
+    loading,
+    refetch,
+  };
+};
