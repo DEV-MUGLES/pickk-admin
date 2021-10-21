5097f218914563fecc5645fec5cc23cdba9f4ea0
syoung125 <laura120555@gmail.com>
2021-10-21 12:45:03 +0900

    chore: remove claimStatus column from placements table

--- src/components/placements/table/excel-columns.ts
+++ src/components/placements/table/excel-columns.ts
@@ -2,7 +2,6 @@ import {FlattenPlacementDataType} from '@containers/placements/hooks';
 import {
   generateExcelColumns,
   addDashToPhoneNumber,
-  getOrderItemClaimStatusDisplayName,
   getOrderItemStatusDisplayName,
   renderDateWithTime,
 } from '@src/common/helpers';
@@ -17,8 +16,6 @@ export const placementsExcelValueMapper: Record<
   confirmedAt: ({confirmedAt}) => renderDateWithTime(confirmedAt),
   status: ({status, isConfirmed}) =>
     getOrderItemStatusDisplayName(status, isConfirmed),
-  claimStatus: ({claimStatus}) =>
-    getOrderItemClaimStatusDisplayName(claimStatus),
   buyerPhoneNumber: ({buyerPhoneNumber}) =>
     addDashToPhoneNumber(buyerPhoneNumber),
   receiverPhoneNumber: ({receiverPhoneNumber}) =>
