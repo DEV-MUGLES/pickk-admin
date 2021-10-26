4d9e8275f305d98b4866ef3294a821b5af8f7637
greatSumini <greatSumini@gmail.com>
2021-10-27 04:57:09 +0900

    feat: :sparkles: add pages/_middleware
    _app.tsx의getInitialProps를 대체합니다!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import App, {AppContext, AppProps} from 'next/app';
+import type {AppProps} from 'next/app';
 import Head from 'next/head';
 import {useRouter} from 'next/router';
-import {ApolloProvider, gql} from '@apollo/client';
+import {ApolloProvider} from '@apollo/client';
 import {BackTop} from 'antd';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import isBetween from 'dayjs/plugin/isBetween';
 import 'dayjs/locale/ko';
 import 'antd/dist/antd.css';
-import {UserRole} from '@pickk/common';
 
 import MainLayout from '@src/components/common/templates/MainLayout';
 
 import {GlobalStyle} from '@src/common/styles';
-import {createApolloClient, useApolloClient} from '@src/providers/apollo';
+import {useApolloClient} from '@src/providers/apollo';
 
 dayjs.extend(relativeTime);
 dayjs.extend(isBetween);
@@ -44,47 +43,3 @@ function PickkAdminApp({Component, pageProps}: AppProps) {
 }
 
 export default PickkAdminApp;
-
-PickkAdminApp.getInitialProps = async (appContext: AppContext) => {
-  // calls page's `getInitialProps` and fills `appProps.pageProps`
-  const appProps = await App.getInitialProps(appContext);
-
-  if (
-    appContext.ctx.pathname === '/login' ||
-    appContext.ctx.pathname === '/cheat-login'
-  ) {
-    return appProps;
-  }
-
-  try {
-    const client = createApolloClient(appContext.ctx.req);
-    const {
-      data: {me},
-    } = await client.query<{me: {role: UserRole}}>({
-      query: GET_ME,
-    });
-
-    if (!me || (me.role !== UserRole.Seller && me.role !== UserRole.Admin)) {
-      throw new Error('권한 없음');
-    }
-
-    return {...appProps, pageProps: {...appProps.pageProps, me}};
-  } catch {
-    const {ctx} = appContext;
-
-    if (ctx.res) {
-      ctx.res.writeHead(302, {Location: `/login?to=${ctx.pathname}`});
-      ctx.res.end();
-    }
-  }
-};
-
-const GET_ME = gql`
-  query me {
-    me {
-      id
-      nickname
-      role
-    }
-  }
-`;
