ea02563ad0212fd955d85af8e0730e90fed7105e
greatSumini <greatSumini@gmail.com>
2021-10-27 16:46:55 +0900

    perf: :zap: remove @pickk/common dependency from _middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,4 @@
 import {NextRequest, NextResponse} from 'next/server';
-import {UserRole} from '@pickk/common';
 
 export async function middleware(req: NextRequest) {
   const {pathname} = req.nextUrl;
@@ -24,7 +23,7 @@ export async function middleware(req: NextRequest) {
       body: JSON.stringify({query: GET_ME}),
     }).then(async (response) => await response.json());
 
-    if (!me || me.role !== UserRole.Seller) {
+    if (!me || me.role !== 'Seller') {
       throw new Error('권한 없음');
     }
 
